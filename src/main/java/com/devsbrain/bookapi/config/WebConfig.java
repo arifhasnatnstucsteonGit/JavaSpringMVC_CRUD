b64f08bc5d6671be9ed9be39faa7b5728e339276
ADN Digital <arif.adndigital@gmail.com>
2018-11-13 19:01:09 +0600

    spring mvc

---
+++ src/main/java/com/devsbrain/bookapi/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.devsbrain.bookapi.config;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "com.devsbrain.bookapi.controller" })
+public class WebConfig extends WebMvcConfigurerAdapter {
+	
+}
